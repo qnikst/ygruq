90e80a26c6807a0498d1c1c290e60decd6973e29
Alexander Vershilov <alexander.vershilov@gmail.com>
2012-05-29 21:26:57 +0400

    add Handler stuff
    Fixes

---
+++ Handler/Quote.hs
@@ -0,0 +1,17 @@
+module Handler.Quote
+    where
+
+import Import
+
+postQuoteCreateR :: Handler RepHtml
+postQuoteCreateR = undefined
+getQuoteCreateR  :: Handler RepHtml
+getQuoteCreateR  = undefined
+getQuoteListR    :: Handler RepHtml
+getQuoteListR    = undefined
+getQuoteListPageR:: Int -> Handler RepHtml
+getQuoteListPageR page = undefined
+getQuoteAbyssListR:: Handler RepHtml
+getQuoteAbyssListR     = undefined
+getQuoteAbyssListPageR::Int-> Handler RepHtml
+getQuoteAbyssListPageR  page  = undefined
