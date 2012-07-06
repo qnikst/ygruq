f58435bbe7b76dd4c7be2c6474dd3ed66bbaa315
Alexander Vershilov <alexander.vershilov@gmail.com>
2012-07-06 22:44:10 +0400

    fix

--- Handler/Quote.hs
+++ Handler/Quote.hs
@@ -112,7 +112,7 @@ quoteList :: QuotePage -> Handler RepHtml
 quoteList quotepage   = do
     search <- runInputGet $ iopt textField "search"
     let q = (QuoteApproved ==. isApproved quotepage):(searchQ search)
-        s = [Desc QuoteTemestamp]
+        s = [Desc QuoteTimestamp]
     (page,onOne) <- runInputGet $ (,) <$> iopt intField "page" <*> iopt intField "all"
     maid <- case quotepage of
               Abyss -> maybeAuth
